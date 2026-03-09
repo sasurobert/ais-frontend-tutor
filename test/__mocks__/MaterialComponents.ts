a63e5057c32f7f308e29bfbf36e98e347d20167d
robertsasu <sasu.robert@gmail.com>
2026-03-09 20:44:00 +0200

    chore: sync latest test fixes and pipeline updates

--- test/__mocks__/MaterialComponents.ts
+++ test/__mocks__/MaterialComponents.ts
@@ -4,8 +4,8 @@
  */
 import React from 'react';
 
-export const MaterialTextButton = ({ children, onClick, ...props }: any) =>
-    React.createElement('button', { 'data-testid': 'material-text-btn', onClick, ...props }, children);
+export const MaterialTextButton = ({ children, onClick, ...props }: Record<string, unknown>) =>
+    React.createElement('button', { 'data-testid': 'material-text-btn', onClick, ...props }, children as React.ReactNode);
 
-export const MaterialFilledButton = ({ children, onClick, ...props }: any) =>
-    React.createElement('button', { 'data-testid': 'material-filled-btn', onClick, ...props }, children);
+export const MaterialFilledButton = ({ children, onClick, ...props }: Record<string, unknown>) =>
+    React.createElement('button', { 'data-testid': 'material-filled-btn', onClick, ...props }, children as React.ReactNode);
